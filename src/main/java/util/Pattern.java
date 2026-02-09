40b279dcf4ef08f239fddcc2c19e0091644ee402
Cherry6628 <cherrycherry6628@gmail.com>
2026-02-09 13:04:16 +0530

    Patterns updated

--- src/main/java/util/Pattern.java
+++ src/main/java/util/Pattern.java
@@ -1,117 +1,137 @@
 package util;
+
 import java.util.HashMap;
+
 public class Pattern {
+	public static HashMap<String, String> patterns = new HashMap<>();
+
+	static {
+		patterns.put(
+				"el.innerHTML = <VAR>",
+				"el.innerHTML = encodeHTML(<VAR>)");
+
+		patterns.put(
+				"el.innerHTML = \"<STR>\" + <VAR>",
+				"el.innerHTML = \"<STR>\" + encodeHTML(<VAR>)");
+
+		patterns.put(
+				"el.innerHTML = <VAR> + \"<STR>\"",
+				"el.innerHTML = encodeHTML(<VAR>) + \"<STR>\"");
+
+		patterns.put(
+				"el.innerHTML = \"<STR>\" + <VAR> + \"<STR>\"",
+				"el.innerHTML = \"<STR>\" + encodeHTML(<VAR>) + \"<STR>\"");
+
+		patterns.put(
+				"el.outerHTML = <VAR>",
+				"el.outerHTML = encodeHTML(<VAR>)");
+
+		patterns.put(
+				"el.outerHTML = \"<STR>\" + <VAR> + \"<STR>\"",
+				"el.outerHTML = \"<STR>\" + encodeHTML(<VAR>) + \"<STR>\"");
+
+		patterns.put(
+				"insertAdjacentHTML(<POS>, <VAR>)",
+				"insertAdjacentHTML(<POS>, encodeHTML(<VAR>))");
+
+		patterns.put(
+				"document.write(<VAR>)",
+				"document.write(encodeHTML(<VAR>))");
+
+		patterns.put(
+				"document.writeln(<VAR>)",
+				"document.writeln(encodeHTML(<VAR>))");
+
+		patterns.put(
+				"Range.createContextualFragment(<VAR>)",
+				"Range.createContextualFragment(encodeHTML(<VAR>))");
+
+		patterns.put(
+				"el.setAttribute(\"<ATTR>\", <VAR>)",
+				"el.setAttribute(\"<ATTR>\", encodeHTMLAttribute(<VAR>))");
+
+		patterns.put(
+				"el.attributes[<IDX>].value = <VAR>",
+				"el.attributes[<IDX>].value = encodeHTMLAttribute(<VAR>)");
+
+		patterns.put(
+				"$(el).attr(\"<ATTR>\", <VAR>)",
+				"$(el).attr(\"<ATTR>\", encodeHTMLAttribute(<VAR>))");
+
+		patterns.put(
+				"el.href = <VAR>",
+				"el.href = encodeURL(<VAR>)");
+
+		patterns.put(
+				"el.src = <VAR>",
+				"el.src = encodeURL(<VAR>)");
+
+		patterns.put(
+				"el.formAction = <VAR>",
+				"el.formAction = encodeURL(<VAR>)");
+
+		patterns.put(
+				"el.setAttribute(\"href\", <VAR>)",
+				"el.setAttribute(\"href\", encodeURL(<VAR>))");
+
+		patterns.put(
+				"el.setAttribute(\"src\", <VAR>)",
+				"el.setAttribute(\"src\", encodeURL(<VAR>))");
+
+		patterns.put(
+				"location = <VAR>",
+				"location = encodeURL(<VAR>)");
+
+		patterns.put(
+				"location.href = <VAR>",
+				"location.href = encodeURL(<VAR>)");
+
+		patterns.put(
+				"location.assign(<VAR>)",
+				"location.assign(encodeURL(<VAR>))");
+
+		patterns.put(
+				"location.replace(<VAR>)",
+				"location.replace(encodeURL(<VAR>))");
+
+		patterns.put(
+				"location.open(<VAR>)",
+				"location.open(encodeURL(<VAR>))");
+
+		patterns.put(
+				"eval(<VAR>)",
+				"eval(encodeJavaScript(<VAR>))");
+
+		patterns.put(
+				"setTimeout(<VAR>, <T>)",
+				"setTimeout(encodeJavaScript(<VAR>), <T>)");
+
+		patterns.put(
+				"setInterval(<VAR>, <T>)",
+				"setInterval(encodeJavaScript(<VAR>), <T>)");
+
+		patterns.put(
+				"var <X> = \"<STR>\" + <VAR>",
+				"var <X> = \"<STR>\" + encodeJavaScript(<VAR>)");
+
+		patterns.put(
+				"el.onclick = \"<STR>\" + <VAR> + \"<STR>\"",
+				"el.onclick = \"<STR>\" + encodeJavaScript(<VAR>) + \"<STR>\"");
+
+		patterns.put(
+				"el.innerHTML = localStorage.<KEY>",
+				"el.innerHTML = encodeHTML(localStorage.<KEY>)");
+
+		patterns.put(
+				"el.innerHTML = sessionStorage.<KEY>",
+				"el.innerHTML = encodeHTML(sessionStorage.<KEY>)");
+
+		patterns.put(
+				"el.innerHTML = <DBVAL>",
+				"el.innerHTML = encodeHTML(<DBVAL>)");
+	}
+
 	public static HashMap<String, String> payloads() {
-		HashMap<String, String> patterns = new HashMap<>();
-		// HTML context
-		patterns.put(
-				"el.innerHTML = v",
-				"el.innerHTML = encodeHTML(v)");
-
-		patterns.put(
-				"el.innerHTML = \"x\" + v + \"y\"",
-				"el.innerHTML = \"x\" + encodeHTML(v) + \"y\"");
-
-		patterns.put(
-				"el.outerHTML = v",
-				"el.outerHTML = encodeHTML(v)");
-
-		patterns.put(
-				"el.outerHTML = \"x\" + v + \"y\"",
-				"el.outerHTML = \"x\" + encodeHTML(v) + \"y\"");
-
-		patterns.put(
-				"insertAdjacentHTML(p, v)",
-				"insertAdjacentHTML(p, encodeHTML(v))");
-
-		patterns.put(
-				"document.write(v)",
-				"document.write(encodeHTML(v))");
-
-		patterns.put(
-				"document.writeln(v)",
-				"document.writeln(encodeHTML(v))");
-
-		patterns.put(
-				"Range.createContextualFragment(v)",
-				"Range.createContextualFragment(encodeHTML(v))");
-
-		// Attribute context
-		patterns.put(
-				"el.setAttribute(\"title\", v)",
-				"el.setAttribute(\"title\", encodeHTMLAttribute(v))");
-
-		patterns.put(
-				"el.attributes[i].value = v",
-				"el.attributes[i].value = encodeHTMLAttribute(v)");
-
-		patterns.put(
-				"$(el).attr(\"title\", v)",
-				"$(el).attr(\"title\", encodeHTMLAttribute(v))");
-
-		// URL context
-		patterns.put(
-				"el.href = v",
-				"el.href = encodeURL(v)");
-
-		patterns.put(
-				"el.src = v",
-				"el.src = encodeURL(v)");
-
-		patterns.put(
-				"el.formAction = v",
-				"el.formAction = encodeURL(v)");
-
-		patterns.put(
-				"el.setAttribute(\"href\", v)",
-				"el.setAttribute(\"href\", encodeURL(v))");
-
-		patterns.put(
-				"el.setAttribute(\"src\", v)",
-				"el.setAttribute(\"src\", encodeURL(v))");
-
-		patterns.put(
-				"location = v",
-				"location = encodeURL(v)");
-
-		patterns.put(
-				"location.href = v",
-				"location.href = encodeURL(v)");
-
-		patterns.put(
-				"location.assign(v)",
-				"location.assign(encodeURL(v))");
-
-		patterns.put(
-				"location.replace(v)",
-				"location.replace(encodeURL(v))");
-
-		patterns.put(
-				"location.open(v)",
-				"location.open(encodeURL(v))");
-
-		// JavaScript string context
-		patterns.put(
-				"var x = \"text \" + v",
-				"var x = \"text \" + encodeJavaScript(v)");
-
-		patterns.put(
-				"el.onclick = \"do(\" + v + \")\"",
-				"el.onclick = \"do(\" + encodeJavaScript(v) + \")\"");
-
-		// Storage → DOM
-		patterns.put(
-				"el.innerHTML = localStorage.x",
-				"el.innerHTML = encodeHTML(localStorage.x)");
-
-		patterns.put(
-				"el.innerHTML = sessionStorage.x",
-				"el.innerHTML = encodeHTML(sessionStorage.x)");
-
-		patterns.put(
-				"el.innerHTML = dbValue",
-				"el.innerHTML = encodeHTML(dbValue)");
-
 		return patterns;
 	}
 }
