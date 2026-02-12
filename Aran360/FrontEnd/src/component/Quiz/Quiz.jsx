20e8576ed3f58b2d191f92fda927f9d9ece6d8ce
Cherry6628 <cherrycherry6628@gmail.com>
2026-02-12 18:26:13 +0530

    Font Sizes Added Variable

--- Aran360/FrontEnd/src/component/Quiz/Quiz.jsx
+++ Aran360/FrontEnd/src/component/Quiz/Quiz.jsx
@@ -3,17 +3,17 @@ import "./Quiz.css";
 import Option from "./Option/Option";
 import Button from "../Button/Button";
 export default function Quiz({ quizId = 0, isCode = true }) {
-  let headline = "Where It Is Commonly Used";
-  let describe =
-    "A CSS property prefixed with -webkit- that works specifically in WebKit-based browsers like Safari and Chrome.";
-  let question = "Which line contain the vulnerabilities?";
-  const option = [
-    "input.innerHTML = userInput",
-    "input.innerHTML = userInput",
-    "input.innerHTML = userInput",
-    "input.innerHTML = userInput",
-  ];
-  const code = `@app.route("/user/<username>")
+    let headline = "Where It Is Commonly Used";
+    let describe =
+        "A CSS property prefixed with -webkit- that works specifically in WebKit-based browsers like Safari and Chrome.";
+    let question = "Which line contain the vulnerabilities?";
+    const option = [
+        "input.innerHTML = userInput",
+        "input.innerHTML = userInput",
+        "input.innerHTML = userInput",
+        "input.innerHTML = userInput",
+    ];
+    const code = `@app.route("/user/<username>")
     def get_user(username):
         db = get_db_connection()
         # Fetch user details safely?
@@ -23,24 +23,33 @@ export default function Quiz({ quizId = 0, isCode = true }) {
         if result:
             return render_template("profile.html", user=result)
         return "User not found", 404`;
-  return (
-    <div className="Quiz">
-      <h1 className="headline">{headline}</h1>
-      <p className="describe">{describe}</p>
-      {isCode && <CodeSnippet code={code}></CodeSnippet>}
-      <p className="question"style={isCode?{
-        fontSize: "20px"
-      }:{
-        fontSize:"50px"
-      }}>{question}</p>
-      <Option name={"name-" + quizId}>{option[0]}</Option>
-      <Option name={"name-" + quizId}>{option[1]}</Option>
-      <Option name={"name-" + quizId}>{option[2]}</Option>
-      <Option name={"name-" + quizId}>{option[3]}</Option>
-      <div className="btns">
-        <Button>Get Answer</Button>
-        <Button>Submit</Button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="Quiz">
+            <h1 className="headline">{headline}</h1>
+            <p className="describe">{describe}</p>
+            {isCode && <CodeSnippet code={code}></CodeSnippet>}
+            <p
+                className="question"
+                style={
+                    isCode
+                        ? {
+                              fontSize: "var(--normal-size)",
+                          }
+                        : {
+                              fontSize: "var(--extra-large-size)",
+                          }
+                }
+            >
+                {question}
+            </p>
+            <Option name={"name-" + quizId}>{option[0]}</Option>
+            <Option name={"name-" + quizId}>{option[1]}</Option>
+            <Option name={"name-" + quizId}>{option[2]}</Option>
+            <Option name={"name-" + quizId}>{option[3]}</Option>
+            <div className="btns">
+                <Button>Get Answer</Button>
+                <Button>Submit</Button>
+            </div>
+        </div>
+    );
 }
