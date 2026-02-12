35e746002840087bcb998240021b83adfbad78d0
Venkatesh-36 <venkateshrajagopal2005@gmail.com>
2026-02-12 14:36:03 +0530

    Quiz Component Added

--- Aran360/FrontEnd/src/component/CodeSnippet/CodeSnippet.jsx
+++ Aran360/FrontEnd/src/component/CodeSnippet/CodeSnippet.jsx
@@ -1,25 +1,15 @@
 import React from "react";
-import './Code.css';
+import './CodeSnippet.css';
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
-function CodeSnippet() {
-  const code = `@app.route("/user/<username>")
-def get_user(username):
-    db = get_db_connection()
-    # Fetch user details safely?
-    query = f"SELECT * FROM users WHERE name = '{username}'"
-    result = db.execute(query).fetchone()
-
-    if result:
-        return render_template("profile.html", user=result)
-    return "User not found", 404`;
+function CodeSnippet({code}) {
   return (
     <div className="wrapper">
       <div className="header">
         <span className="fileName">app.py</span>
         <span className="language">Python 3.10</span>
       </div>
-      <SyntaxHighlighter language="python" style={vscDarkPlus} showLineNumbers className="codeBlock" lineNumberStyle={{ minWidth: "30px", marginRight: "12px", borderRight: "1px solid var( --border-light)", textAlign: "center"}}>
+      <SyntaxHighlighter language="python" style={vscDarkPlus} showLineNumbers  codeTagProps={{ style: { fontSize: "20px" } }} className="codeBlock" lineNumberStyle={{ minWidth: "30px", marginRight: "12px", borderRight: "1px solid var( --border-light)", textAlign: "center"}}>
         {code}
       </SyntaxHighlighter>
     </div>
--- Aran360/FrontEnd/src/component/Quiz/Quiz.jsx
+++ Aran360/FrontEnd/src/component/Quiz/Quiz.jsx
@@ -1,22 +1,46 @@
-import CodeSnippet from '../CodeSnippet/CodeSnippet';
-import './Quiz.css';
-import Option from './Option/Option';
-import Button from '../Button/Button';
-export default function Quiz(){
-    return(
-        <div className="Quiz">
-            <h1 className='headline'>Cross-site scripting</h1>
-            <p className='describe'>In this section, we'll explain what cross-site scripting is, describe the different varieties of cross-site scripting vulnerabilities, and spell out how to find and prevent cross-site scripting.</p>
-            <CodeSnippet></CodeSnippet>
-            <p className='question'>Which line contain the vulnerabilities?</p>
-            <Option></Option>
-            <Option></Option>
-            <Option></Option>
-            <Option></Option>
-            <div className='btns'>
-                <Button>Get Answer</Button>
-                <Button>Submit</Button>
-            </div>
-        </div>
-    );
+import CodeSnippet from "../CodeSnippet/CodeSnippet";
+import "./Quiz.css";
+import Option from "./Option/Option";
+import Button from "../Button/Button";
+export default function Quiz({ quizId = 0, isCode = true }) {
+  let headline = "Where It Is Commonly Used";
+  let describe =
+    "A CSS property prefixed with -webkit- that works specifically in WebKit-based browsers like Safari and Chrome.";
+  let question = "Which line contain the vulnerabilities?";
+  const option = [
+    "input.innerHTML = userInput",
+    "input.innerHTML = userInput",
+    "input.innerHTML = userInput",
+    "input.innerHTML = userInput",
+  ];
+  const code = `@app.route("/user/<username>")
+    def get_user(username):
+        db = get_db_connection()
+        # Fetch user details safely?
+        query = f"SELECT * FROM users WHERE name = '{username}'"
+        result = db.execute(query).fetchone()
+    
+        if result:
+            return render_template("profile.html", user=result)
+        return "User not found", 404`;
+  return (
+    <div className="Quiz">
+      <h1 className="headline">{headline}</h1>
+      <p className="describe">{describe}</p>
+      {isCode && <CodeSnippet code={code}></CodeSnippet>}
+      <p className="question"style={isCode?{
+        fontSize: "20px"
+      }:{
+        fontSize:"50px"
+      }}>{question}</p>
+      <Option name={"name-" + quizId}>{option[0]}</Option>
+      <Option name={"name-" + quizId}>{option[1]}</Option>
+      <Option name={"name-" + quizId}>{option[2]}</Option>
+      <Option name={"name-" + quizId}>{option[3]}</Option>
+      <div className="btns">
+        <Button>Get Answer</Button>
+        <Button>Submit</Button>
+      </div>
+    </div>
+  );
 }
