3c974d2814e7c949a8b1def4186f2b7daa0a80bd
Venkatesh-36 <venkateshrajagopal2005@gmail.com>
2026-02-12 12:02:02 +0530

    Quiz Component Edited

--- Aran360/FrontEnd/src/App.jsx
+++ Aran360/FrontEnd/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
+import Quiz from "./component/Quiz/Quiz";
 import Sidebar from "./component/Sidebar/Sidebar";
-import AuthModal from "./modals/AuthModal/AuthModal";
 
 export default function App() {
     return (
         <>
-            <AuthModal/>
+            {/* <Sidebar/> */}
+            <Quiz></Quiz>
         </>
     );
 }
