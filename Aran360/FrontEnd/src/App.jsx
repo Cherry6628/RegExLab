df5d956706dc9096884ca945b9aea20643aa8138
Cherry6628 <cherrycherry6628@gmail.com>
2026-02-12 16:45:32 +0530

    Header Updated

--- Aran360/FrontEnd/src/App.jsx
+++ Aran360/FrontEnd/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Header from "./component/Header/Header";
 import Quiz from "./component/Quiz/Quiz";
 import Sidebar from "./component/Sidebar/Sidebar";
 import AuthModal from "./modals/AuthModal/AuthModal";
@@ -7,7 +8,7 @@ import AuthPage from "./pages/AuthPage/AuthPage";
 export default function App() {
     return (
         <>
-        <AuthPage/>
+
         </>
     );
 }
