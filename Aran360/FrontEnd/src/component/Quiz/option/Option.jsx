532593588090cd6437a390836eb93b258554839a
Venkatesh-36 <venkateshrajagopal2005@gmail.com>
2026-02-12 10:51:36 +0530

    Quiz Component Added - Not Completed

---
+++ Aran360/FrontEnd/src/component/Quiz/option/Option.jsx
@@ -0,0 +1,9 @@
+import './Option.css';
+export default function Option(){
+    return(
+        <div className="box">
+            <p>input.innerHTML = "userInput";</p>
+            <div className='click'></div>
+        </div>
+    );
+}
