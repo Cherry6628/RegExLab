5bba97785797fec30246043c662bc33dc2756108
Venkatesh-36 <venkateshrajagopal2005@gmail.com>
2026-02-15 13:17:36 +0530

    XSS Learning Material Edited

--- Aran360/FrontEnd/src/App.jsx
+++ Aran360/FrontEnd/src/App.jsx
@@ -12,8 +12,8 @@ export default function App() {
     return (
         <>
             <ContextProvider>
-                {/* <LearningPage/> */}
-                <AuthPage/>
+                <LearningPage/>
+                {/* <AuthPage/> */}
             </ContextProvider>
         </>
     );
