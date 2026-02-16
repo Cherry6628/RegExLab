b0fe6c2609e322892f7e5dc11ec785bb8962e15e
Venkatesh-36 <venkateshrajagopal2005@gmail.com>
2026-02-16 09:32:56 +0530

    Quiz Page created

--- Aran360/FrontEnd/src/component/Quiz/Quiz.jsx
+++ Aran360/FrontEnd/src/component/Quiz/Quiz.jsx
@@ -2,7 +2,7 @@ import CodeSnippet from "../CodeSnippet/CodeSnippet";
 import "./Quiz.css";
 import Option from "./Option/Option";
 import Button from "../Button/Button";
-export default function Quiz({ quizId = 0, isCode = true }) {
+export default function Quiz({ quizId = 0, isCode = false }) {
     let headline = "Where It Is Commonly Used";
     let describe =
         "A CSS property prefixed with -webkit- that works specifically in WebKit-based browsers like Safari and Chrome.";
@@ -13,16 +13,16 @@ export default function Quiz({ quizId = 0, isCode = true }) {
         "input.innerHTML = userInput",
         "input.innerHTML = userInput",
     ];
-    const code = `@app.route("/user/<username>")
-    def get_user(username):
-        db = get_db_connection()
-        # Fetch user details safely?
-        query = f"SELECT * FROM users WHERE name = '{username}'"
-        result = db.execute(query).fetchone()
+    // const code = `@app.route("/user/<username>")
+    // def get_user(username):
+    //     db = get_db_connection()
+    //     # Fetch user details safely?
+    //     query = f"SELECT * FROM users WHERE name = '{username}'"
+    //     result = db.execute(query).fetchone()
     
-        if result:
-            return render_template("profile.html", user=result)
-        return "User not found", 404`;
+    //     if result:
+    //         return render_template("profile.html", user=result)
+    //     return "User not found", 404`;
     return (
         <div className="Quiz">
             <h1 className="headline">{headline}</h1>
@@ -37,6 +37,8 @@ export default function Quiz({ quizId = 0, isCode = true }) {
                           }
                         : {
                               fontSize: "var(--extra-large-size)",
+                              textAlign:"center",
+                              marginBottom:"40px",
                           }
                 }
             >
