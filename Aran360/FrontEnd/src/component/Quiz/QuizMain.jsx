e8e23c86b97e359dcbbcb0f4680f08f48340bedf
Venkatesh-36 <venkateshrajagopal2005@gmail.com>
2026-02-17 11:07:35 +0530

    Quiz Page Edited

--- Aran360/FrontEnd/src/component/Quiz/QuizMain.jsx
+++ Aran360/FrontEnd/src/component/Quiz/QuizMain.jsx
@@ -1,16 +1,26 @@
-import Vulnerable from "./vulnerability/Vulnerability";
+import VulnerabilityTopic from "./VulnerabilityTopic/VulnerabilityTopic";
 import './QuizMain.css';
+import { useState } from "react";
+import QuizContainer from "./QuizContainer";
 export default function QuizMain(){
+    const[display,setDisplay]=useState(0)
     return(
-        <div id="QuizMain">
-            <main className="content">
+        <div id="QuizMain" style={{width: "calc(100vw - 320px)",height: "calc(100vh - 180px)"}}>
+            {display==0&&<main className="content">
                 <h1>What would you like to master today?</h1>
                 <p>Select a vulnerability domain to begin your interactive training session.Each<br/>path contains hands-onlabs and theoretical assesments.</p>
                 <div className="separate">
-                    <Vulnerable icon={<span class="material-symbols-outlined">terminal</span>} heading="Cross-Site Scripting (XSS)" content="Cross Site Scripting (XSS) allows attackers to inject malicious JavaScript into web pages. When other users visit the page, the script executes in their browser and can steal cookies, session tokens, or sensitive data." />
-                    <Vulnerable icon={<span class="material-symbols-outlined">database</span>} heading="SQL Injection" content="SQL Injection is a vulnerability where attackers insert malicious SQL queries into input fields to manipulate or access unauthorized database data."/>
+                    <VulnerabilityTopic onClick={()=>setDisplay(1)} icon="terminal" heading="Cross-Site Scripting (XSS)" content="Cross Site Scripting (XSS) allows attackers to inject malicious JavaScript into web pages. When other users visit the page, the script executes in their browser and can steal cookies, session tokens, or sensitive data." />
+                    <VulnerabilityTopic icon="database" heading="SQL Injection" content="SQL Injection is a vulnerability where attackers insert malicious SQL queries into input fields to manipulate or access unauthorized database data."/>
+                    <VulnerabilityTopic icon="terminal" heading="Cross-Site Scripting (XSS)" content="Cross Site Scripting (XSS) allows attackers to inject malicious JavaScript into web pages. When other users visit the page, the script executes in their browser and can steal cookies, session tokens, or sensitive data." />
                 </div>
-            </main>
+                <div className="separate">
+                    <VulnerabilityTopic onClick={()=>setDisplay(1)} icon="terminal" heading="Cross-Site Scripting (XSS)" content="Cross Site Scripting (XSS) allows attackers to inject malicious JavaScript into web pages. When other users visit the page, the script executes in their browser and can steal cookies, session tokens, or sensitive data." />
+                    <VulnerabilityTopic icon="database" heading="SQL Injection" content="SQL Injection is a vulnerability where attackers insert malicious SQL queries into input fields to manipulate or access unauthorized database data."/>
+                    <VulnerabilityTopic icon="terminal" heading="Cross-Site Scripting (XSS)" content="Cross Site Scripting (XSS) allows attackers to inject malicious JavaScript into web pages. When other users visit the page, the script executes in their browser and can steal cookies, session tokens, or sensitive data." />
+                </div>
+            </main>}
+            {display==1&&<QuizContainer/>}
         </div>
     );
 }
