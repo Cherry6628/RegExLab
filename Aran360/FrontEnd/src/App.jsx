bf649a73068056deacb1ef9aee422a14c94dab6b
Venkatesh-36 <venkateshrajagopal2005@gmail.com>
2026-02-17 15:08:30 +0530

    Progress Bar Edited - Not Completed

--- Aran360/FrontEnd/src/App.jsx
+++ Aran360/FrontEnd/src/App.jsx
@@ -5,6 +5,9 @@ import {BrowserRouter, Routes, Route } from "react-router-dom";
 import { frontendbasename } from "./utils/params";
 import XSSMain from "./component/LearningMaterials/XSS/XSSMain";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import QuizMain from "./component/Quiz/QuizMain";
+import NotFound_404 from "./pages/NotFound_404/NotFound_404";
+import QuizResult from "./component/Quiz/QuizResult";
 export default function App() {
     return (
         <>
@@ -14,7 +17,9 @@ export default function App() {
                         <Route path="/learning-material/xss" element={<XSSMain/>}/>
                         <Route path="/accounts" element={<AuthPage/>}/>
                         <Route path="/dashboard" element={<Dashboard/>}/>
-                        <Route path="*" element={<Dashboard/>}/>
+                        <Route path="/test" element={<QuizMain/>}/>
+                        <Route path="/result" element={<QuizResult/>}/>
+                        <Route path="*" element={<NotFound_404/>}/>
                     </Routes>
                 </BrowserRouter>
             </ContextProvider>
