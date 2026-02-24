9722fa0ab1792739ea1a8325f420485f1942c385
Cherry6628 <cherrycherry6628@gmail.com>
2026-02-24 12:38:30 +0530

    Packages Version Updated

---
+++ Aran360/BackEnd/src/main/java/controller/servlets/quiz/QuizQuestionServlet.java
@@ -0,0 +1,30 @@
+package controller.servlets.quiz;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import org.json.JSONObject;
+@WebServlet("/quiz-questions")
+public class QuizQuestionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader reader = request.getReader();
+		String line;
+		while ((line = reader.readLine()) != null)
+			sb.append(line);
+		JSONObject body = new JSONObject(sb.toString());
+		String title = body.getString("title");
+		if(title==null||title.isBlank()) {
+			response.sendRedirect("/test");
+		}
+		//TODO
+	}
+
+}
---
+++ Aran360/BackEnd/src/main/java/controller/servlets/quiz/QuizResultServlet.java
@@ -0,0 +1,28 @@
+package controller.servlets.quiz;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import org.json.JSONObject;
+
+@WebServlet("/QuizResultServlet")
+public class QuizResultServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader reader = request.getReader();
+		String line;
+		while ((line = reader.readLine()) != null)
+			sb.append(line);
+		JSONObject body = new JSONObject(sb.toString());
+		
+	}
+
+}
